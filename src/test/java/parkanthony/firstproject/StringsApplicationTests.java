15ef7c6dc4c14dd650432a4b695473c727a12398
ParkAnthony13 <anth0nypark13@gmail.com>
2021-08-06 01:54:52 -0700

    first commit

---
+++ src/test/java/parkanthony/firstproject/StringsApplicationTests.java
@@ -0,0 +1,13 @@
+package parkanthony.firstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
